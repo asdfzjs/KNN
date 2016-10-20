48448f14a5a84df34de8ee8450ac5ccf812db00c
unknown <zhujisong@zibang.com>
2016-10-20 14:19:54 +0800

    aaa

--- kNN.py
+++ kNN.py
@@ -9,17 +9,23 @@ def createDataSet():
 
 #用于分类的输入向量：inX,输入的训练样本集合为：dataSet,标签向量为:labels,最后的参数k是用于选择最近邻居的数目
 def classify0(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0]   #shape[0]就是读取矩阵第一维度的长度
+    dataSetSize = dataSet.shape[0]   #shape[0]就是读取矩阵第一维度的长度,计算矩阵有多少行
+ #   print tile(inX, (dataSetSize,1))  #生成4行一列的矩阵
     diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    print diffMat
     sqDiffMat = diffMat**2
-    sqDistances = sqDiffMat.sum(axis=1)
-    distances = sqDistances**0.5
-    sortedDistIndicies = distances.argsort()
-    classCount={}
+    print sqDiffMat
+    sqDistances = sqDiffMat.sum(axis=1) #将一个矩阵的每一行向量相加
+    print sqDistances
+    distances = sqDistances**0.5    #开方
+    print distances
+    sortedDistIndicies = distances.argsort()   #argsort函数返回的是数组值从小到大的索引值
+    print sortedDistIndicies
+    classCount={}  #定义了一个字典
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)   #operator.itemgetter(1)取下标索引值是1的进行排序，true是降序
     return sortedClassCount[0][0]
 
 
