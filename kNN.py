2ee8b878827d72345a82b103b4863a93506afde9
unknown <zhujisong@zibang.com>
2016-10-21 17:59:51 +0800

    KNN

--- kNN.py
+++ kNN.py
@@ -1,6 +1,7 @@
 #-*-coding:utf-8-*-
 from numpy import *
 import operator
+from os import listdir
 #收集数据，准备数据，分析数据，训练算法，测试算法，使用算法
 
 def createDataSet():
@@ -56,11 +57,58 @@ def autoNorm(dataSet):
     normDataSet = normDataSet/tile(ranges,(m,1))  #数据相除，归一化
     return normDataSet,ranges,minVals
 
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
+    normMat,ranges,minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        print "the classifier came back with: %d,the real answer is :%d" % (classifierResult,datingLabels[i])
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print "the total error rate is: %f" % (errorCount / float(numTestVecs))
+    print errorCount
+def img2vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')           #load the training set
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]     #take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')        #iterate through the test set
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]     #take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr)
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print "\nthe total number of errors is: %d" % errorCount
+    print "\nthe total error rate is: %f" % (errorCount/float(mTest))
 
 if __name__=="__main__":
    # group,labels = createDataSet()
    #print classify0([0,0],group,labels,3)
-   datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
-   print datingDataMat[0:20]
-   autoNorm(datingDataMat)
+   #datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
+   #print datingDataMat[0:20]
+   #autoNorm(datingDataMat)
+   datingClassTest()
 
